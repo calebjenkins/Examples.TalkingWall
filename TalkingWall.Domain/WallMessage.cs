2fc44ae64888fd9b8f8b8428f677f5efe87bc54d
calebjenkins <caleb@calebjenkins.com>
2014-05-29 15:28:08 -0500

    Updating domain to include Name

---
+++ TalkingWall.Domain/WallMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalkingWall.Domain
+{
+    public class WallMessage
+    {
+        public DateTime TimeStamp { get; set; }
+        public string Name { get; set; }
+        public string Message { get; set; }
+
+    }
+}
