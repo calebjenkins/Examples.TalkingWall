f127d12e04e5a278b94d2e7d29e8c32830edf41e
calebjenkins <caleb@calebjenkins.com>
2014-05-30 14:14:42 -0500

    Adding boostrap files and masterpage - posts from menu

---
+++ TalkingWall/default_.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TalkingWall.Domain;
+
+namespace TalkingWall
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                RefreshData();
+            }
+        }
+
+        protected void PostButton_Click(object sender, EventArgs e)
+        {
+            var msg = new WallMessage() { Message = WallInput.Text, Name=WallName.Text, TimeStamp = DateTime.Now };
+            Global.Messages.Add(msg);
+            RefreshData();
+        }
+
+        private void RefreshData()
+        {
+            WallGrid.DataSource = Global.Messages.Reverse();
+            WallGrid.DataBind();
+
+            WallRepeater.DataSource = Global.Messages.Reverse();
+            WallRepeater.DataBind();
+        }
+    }
+}
