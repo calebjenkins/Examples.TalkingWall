468b5021e835b1e28cf14724caca7b58c8899f2d
calebjenkins <caleb@calebjenkins.com>
2014-06-14 00:11:02 -0500

    Adding MVC Unit Tests..

---
+++ TalkingWall.UI.MVC.Tests/When_WallMessageController_Index_.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TalkingWall.Controllers;
+using TalkingWall.Domain.Services;
+using Rhino.Mocks;
+using TalkingWall.Domain;
+using System.Collections.ObjectModel;
+using System.Web.Mvc;
+
+namespace TalkingWall.UI.MVC.Tests
+{
+    [TestClass]
+    public class When_WallMessageController_Index_
+    {
+
+        IWallMessageRepository repo;
+        Collection<WallMessage> messages;
+
+        [TestInitialize]
+        public void setup()
+        {
+            repo = MockRepository.GenerateMock<IWallMessageRepository>();
+            messages = new Collection<WallMessage>() {
+                new WallMessage() { Message="Test 1", Name="Joe Smith", TimeStamp = DateTime.MinValue}
+            };
+        }
+
+        [TestMethod]
+        public void should_data_from_repo()
+        {
+            
+            repo.Expect(x => x.Messages).Return(messages).Repeat.Once();
+
+            WallController controller = new WallController(repo);
+            var view = controller.Index() as ViewResult;
+            var model = (Collection<WallMessage>) view.ViewData.Model;
+
+            Assert.AreEqual(1, model.Count);
+            Assert.AreEqual("Test 1", model[0].Message);
+
+            repo.VerifyAllExpectations();
+        }
+    }
+}
